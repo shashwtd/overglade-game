7db575a8cbc303dad32c85747abc73330992667d
Shashwat Dubey <shashwat5590@gmail.com>
2026-01-13 16:05:47 +0530

    I added a plane, a character model, some collisions, basic setup

---
+++ 0yak_scripts/player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody3D
+
+
+const SPEED = 4.0
+const JUMP_VELOCITY = 3.5
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, direction.x * SPEED, 10 * delta)
+		velocity.z = move_toward(velocity.z, direction.z * SPEED, 10 * delta)
+
+
+	move_and_slide()
